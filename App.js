4002ba24b1be5dc105f1ba3074475dc1edb45f22
unknown <zachariah11jerobert@gmail.com>
2021-09-08 10:13:26 +0530

    2.1_components profile

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {View,Text} from 'react-native';
+import {View, Text} from 'react-native';
+import Profile from './components/Profile';
 
-const App = () =>{
-  return(
-    <View>
-      <Text style={{fontSize:100}}>Hello React-Native</Text>
+const App = () => {
+  return (
+    <View style={{flex: 1, margin: 40}}>
+      <Text style={{fontSize: 40}}>Components</Text>
+      <Profile />
+      <Profile />
     </View>
-  )
-}
+  );
+};
 
 export default App;
---
+++ components/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Profile=()=>{
+    return(
+        <View>
+            <Text style={{fontSize:50}}>Profile Component</Text>
+        </View>
+    )
+}
+
+export default Profile;
