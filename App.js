9572f7758f071574c8f9aee310deb10cc86118c6
unknown <zachariah11jerobert@gmail.com>
2021-09-08 11:23:45 +0530

    5.state using useState

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, Button} from 'react-native';
 
-const App =()=>{
-  return(
-    <View>
-      <Text style={{fontSize:40}}>Functional Component</Text>
+const App = () => {
+  const [data, setData] = useState(0);
+  return (
+    <View style={{margin: 20}}>
+      <Button onPress={() => setData(data - 1)} title="Decrease" />
+
+      <Text style={{fontSize: 100}}>{data}</Text>
+
+      <Button onPress={() => setData(data + 1)} title="Increase" />
     </View>
-  )
-}
+  );
+};
 
 export default App;
