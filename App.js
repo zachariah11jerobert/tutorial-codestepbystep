f8e5544318488516f4dfa3e048d8e3ec0ed115a0
unknown <zachariah11jerobert@gmail.com>
2021-09-08 10:52:51 +0530

    4.functional component

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
-import Profile from './components/Profile';
 
-class App extends React.Component{
- render(){
-   return(
-     <View>
-       <Text style={{fontSize:50}}>Class Component</Text>
-       <Profile/>
-     </View>
-   )
- }
+const App =()=>{
+  return(
+    <View>
+      <Text style={{fontSize:40}}>Functional Component</Text>
+    </View>
+  )
 }
 
 export default App;
