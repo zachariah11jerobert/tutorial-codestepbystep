677c3c43393709d8a6d3f67e38790b9831c5eeb0
unknown <zachariah11jerobert@gmail.com>
2021-09-09 08:42:42 +0530

    10.2 making modal with button controlls

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {View, Text, Modal} from 'react-native';
+import {View, Text, Modal, Button} from 'react-native';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+  }
+
   render() {
     return (
       <View style={{flex: 1, marginTop: 100, backgroundColor: 'yellow'}}>
         <Text style={{fontSize: 80}}>Normal Screen Text</Text>
-
-        <Modal transparent={true} visible={true}>
+        <Button title="show modal" onPress={() => this.setState({show: true})} />
+        <Modal transparent={true} visible={this.state.show}>
           <View style={{backgroundColor: '#000000aa', flex: 1}}>
             <View
               style={{
@@ -18,6 +25,7 @@ class App extends React.Component {
                 flex: 1,
               }}>
               <Text style={{fontSize: 50}}>Modal Text</Text>
+              <Button title="close modal" onPress={() => this.setState({show: false})} />
             </View>
           </View>
         </Modal>
