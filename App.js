e199a6930c8cb5e5264cdac3ccc86ed472c08047
unknown <zachariah11jerobert@gmail.com>
2021-09-09 12:44:14 +0530

    disable arrows

--- App.js
+++ App.js
@@ -6,34 +6,7 @@ import {Calendar} from 'react-native-calendars';
 const App = () => {
   return (
     <>
-      <Calendar
-        dayComponent={e => {
-          return (
-            <View>
-              <Text>1</Text>
-            </View>
-          );
-        }}
-      />
-
-      <Calendar
-        dayComponent={e => {
-          return (
-            <View
-              style={{
-                width: 25,
-                height: 25,
-                borderRadius: 12.5,
-                backgroundColor: 'lightgreen',
-                display:"flex",
-                justifyContent:"center",
-                alignItems:"center"
-              }}>
-              <Text>{e.date.day}</Text>
-            </View>
-          );
-        }}
-      />
+      <Calendar disableArrowLeft disableArrowRight />
     </>
   );
 };
