b830b365841d03dff1e885b003712a2d509a95da
unknown <zachariah11jerobert@gmail.com>
2021-09-09 12:41:46 +0530

    understanding calendar dayComponent

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import {Calendar} from 'react-native-calendars';
 
 const App = () => {
   return (
     <>
-      <Calendar />
+      <Calendar
+        dayComponent={e => {
+          return (
+            <View>
+              <Text>1</Text>
+            </View>
+          );
+        }}
+      />
+
+      <Calendar
+        dayComponent={e => {
+          return (
+            <View
+              style={{
+                width: 25,
+                height: 25,
+                borderRadius: 12.5,
+                backgroundColor: 'lightgreen',
+                display:"flex",
+                justifyContent:"center",
+                alignItems:"center"
+              }}>
+              <Text>{e.date.day}</Text>
+            </View>
+          );
+        }}
+      />
     </>
   );
 };
